2777144b7ed1125fc206e7f8bf2c76c937cb4186
Sean Oelkers <sean@thqk.com>
2016-05-13 15:05:31 -0500

    removed unnecessary js links

--- cipher.php
+++ cipher.php
@@ -333,6 +333,4 @@ function cipherDecode($string) {
 			</form>
 		</div>
 		</body>
-		<script src='http://code.jquery.com/jquery-1.11.3.min.js'></script>
-		<script src='https://cdn.jsdelivr.net/bootstrap/3.3.5/js/bootstrap.min.js'></script>
 		</html>
